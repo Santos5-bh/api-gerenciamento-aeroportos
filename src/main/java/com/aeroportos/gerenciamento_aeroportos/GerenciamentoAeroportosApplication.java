2fb834a3e32447c5bd517ec61926a7fbfb428956
Gabriel Santos Martins <gabriel3977martins@gmail.com>
2025-11-19 12:13:35 -0300

    feat: inicializa estrutura do projeto Spring Boot com MySQL e JPA

---
+++ src/main/java/com/aeroportos/gerenciamento_aeroportos/GerenciamentoAeroportosApplication.java
@@ -0,0 +1,13 @@
+package com.aeroportos.gerenciamento_aeroportos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoAeroportosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoAeroportosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aeroportos/gerenciamento_aeroportos/GerenciamentoAeroportosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeroportos.gerenciamento_aeroportos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoAeroportosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
